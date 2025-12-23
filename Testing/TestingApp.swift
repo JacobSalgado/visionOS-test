3b29ba3cb844e36701bd75dd775d548798261711
JacobSalgado <jrs244@njit.edu>
2025-12-23 14:01:37 -0500

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,33 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Research on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ TestingTests/TestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTests.swift
+//  TestingTests
+//
+//  Created by Research on 12/23/25.
+//
+
+import Testing
+@testable import Testing
+
+struct TestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
